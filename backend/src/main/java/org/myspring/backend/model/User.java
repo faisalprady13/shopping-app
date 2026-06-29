1b8286c7ab04cf11a1590b656d96eb9a475a38ec
m-reinisch <michael.reinisch@email.de>
2026-06-29 14:45:51 +0200

    User implemented

---
+++ backend/src/main/java/org/myspring/backend/model/User.java
@@ -0,0 +1,23 @@
+package org.myspring.backend.model;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.Id;
+import jakarta.persistence.OneToMany;
+import jakarta.persistence.Table;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import java.util.List;
+
+@Entity
+@Table( name= "Users" )
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+public class User {
+    @Id
+    private String id;
+    private String name;
+    @OneToMany(mappedBy = "user")
+    private List<ShoppingList> shoppingLists;
+}
