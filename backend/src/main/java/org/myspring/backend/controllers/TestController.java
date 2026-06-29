1dda01e5dbcc66167f30d47417ccf583214b7665
faisalprady13 <faisalprady@gmail.com>
2026-06-29 14:12:53 +0200

    test

---
+++ backend/src/main/java/org/myspring/backend/controllers/TestController.java
@@ -0,0 +1,19 @@
+package org.myspring.backend.controllers;
+
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/api/message")
+class TestController {
+
+    @GetMapping
+    String getMessage() {
+        System.out.println("getMessage");
+        System.out.println("getMessage");
+        System.out.println("getMessage");
+        return "Hello World!";
+    }
+}
