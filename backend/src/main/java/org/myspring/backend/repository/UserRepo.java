13bb95a37a2ad559828d8a55e5ebee14bf0a0b65
faisalprady13 <faisalprady@gmail.com>
2026-07-03 18:37:45 +0200

    clean up

--- backend/src/main/java/org/myspring/backend/repository/UserRepo.java
+++ backend/src/main/java/org/myspring/backend/repository/UserRepo.java
@@ -4,11 +4,9 @@ import org.myspring.backend.model.User;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
-import java.util.List;
-import java.util.UUID;
-
 @Repository
 public interface UserRepo extends JpaRepository<User, String> {
     User findUserById(String id);
+
     User findByName(String name);
 }
